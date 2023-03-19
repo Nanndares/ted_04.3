85f1fe834f0d3083713c7e2b60c6a6ae248b16cd
Nanndares <fernanda.fcsa@hotmail.com>
2023-03-19 16:58:40 -0300

    Initial commit

---
+++ Exercício Messias 3.py
@@ -0,0 +1,23 @@
+print('Operações que resultam em 8')
+
+print("Adição")
+print("4 + 4 =")
+total = int(4+4)
+print(total)
+
+print("Subtração")
+print("16 - 8 =")
+total2 = int(16-8)
+print(total2)
+
+print("Multiplicação")
+print("4 * 2 =")
+total3 = int(4*2)
+print(total3)
+
+print("Divisão")
+print("16 / 2=")
+total4 = int(16/2)
+print(total4)
+
+
